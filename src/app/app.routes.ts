75671ff15748a783005e0ea7f13affe74b664202
yambaolr <yambaolorinrobelle@gmail.com>
2026-02-07 23:23:59 +0800

    ✨ Add navbar and complete portfolio page routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { About } from './about/about';
+import { Projects } from './projects/projects';
+import { Resume } from './resume/resume';
+import { Contact } from './contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: 'about', component: About },
+    { path: 'projects', component: Projects },
+    { path: 'resume', component: Resume },
+    { path: 'contact', component: Contact}
+];
---
+++ src/app/resume/resume.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resume',
+  imports: [],
+  templateUrl: './resume.html',
+  styleUrl: './resume.css',
+})
+export class Resume {
+
+}
