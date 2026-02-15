c682ac008e4d9fcb2e55e367eccb1b25ccffcdad
yambaolr <yambaolorinrobelle@gmail.com>
2026-02-15 15:50:36 +0800

    ✨ Built complete Contact form using EmailJS
    - fully functional form using emailjs
    - contact links
    - error and success messages after submission

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -1,11 +1,56 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true, 
+  imports: [CommonModule, ReactiveFormsModule], 
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })
 export class Contact {
+  contactForm: FormGroup;
+  isSubmitted = false;
+  successMessage = '';
+  errorMessage = '';
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
+  }
+
+  get f() {
+    return this.contactForm.controls;
+  }
+
+  onSubmit() {
+    this.isSubmitted = true;
+    this.successMessage = '';
+    this.errorMessage = '';
+
+    if (this.contactForm.invalid) return;
+
+    emailjs.send(
+      'service_81wv4h3',
+      'template_3b6d96t',
+      {
+        from_name: this.contactForm.value.name,
+        from_email: this.contactForm.value.email,
+        message: this.contactForm.value.message
+      },
+      //public key
+      'o7ih3doHU6B7ypJPy'
+    ).then(() => {
+      this.successMessage = "Message sent successfully!";
+      this.contactForm.reset();
+      this.isSubmitted = false;
+    }).catch(() => {
+      this.errorMessage = "Something went wrong. Please try again.";
+    });
+  }
 }
